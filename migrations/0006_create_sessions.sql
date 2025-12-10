ef5a6fb07d16e14877907bd49cba9a88d492307e
Paul <lazaro.roca.pol@gmail.com>
2025-12-11 00:20:44 +0100

    fix: Sessions FK was referencing wrong table

--- migrations/0006_create_sessions.sql
+++ migrations/0006_create_sessions.sql
@@ -1,7 +1,7 @@
 CREATE TABLE sessions (
     id UUID PRIMARY KEY,
     patient_id UUID NOT NULL REFERENCES patients(id) ON DELETE CASCADE, 
-    professional_id UUID  NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    professional_id UUID  NOT NULL REFERENCES professionals(id) ON DELETE CASCADE,
     session_type_id UUID REFERENCES session_types(id) ON DELETE SET NULL, 
     session_status_id INTEGER NOT NULL,
     session_date TIMESTAMP NOT NULL,
